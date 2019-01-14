0c5e7141762791dacdd92b6f6add270785e3d2b5
Mickaël Misbach <mickael@misba.ch>
2019-01-14 17:16:02 +0100

    deployment enhancements

--- deployment/i2b2-medco/install-medco-cell.sh
+++ deployment/i2b2-medco/install-medco-cell.sh
@@ -5,6 +5,6 @@ set -Eeuo pipefail
 pushd "$MEDCO_CELL_SRC_DIR"
 sed -i "/jboss.home/c\jboss.home=$JBOSS_HOME" build.properties
 sed -i "/medco.unlynx.groupfilepath/c\medco.unlynx.groupfilepath=$MEDCO_CONF_DIR/group.toml" etc/spring/medcoapp/medco.properties
-sed -i "/medco.unlynx.binarypath/c\medco.unlynx.binarypath=$MEDCO_CONF_DIR/unlynxMedCo" etc/spring/medcoapp/medco.properties
+sed -i "/medco.unlynx.binarypath/c\medco.unlynx.binarypath=$MEDCO_CONF_DIR/medco-unlynx" etc/spring/medcoapp/medco.properties
 ant -f build.xml clean all deploy
 popd
